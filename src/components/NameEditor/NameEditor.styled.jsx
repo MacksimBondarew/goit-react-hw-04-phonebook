9823fda8c49b5fd68a66816cc2ec7e4540543874
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-04-27 21:55:54 +0300

    add practice form on hooks-form

--- src/components/NameEditor/NameEditor.styled.jsx
+++ src/components/NameEditor/NameEditor.styled.jsx
@@ -74,3 +74,48 @@ export const AddContactButton = styled.button`
         background-color: #006699;
     }
 `;
+export const Error = styled.div`
+    background-color: #ff6b6b;
+    color: #fff;
+    padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-bottom: 10px;
+`;
+
+export const ErrorIcon = styled.span`
+    font-size: 20px;
+    margin-right: 10px;
+`;
+
+export const ErroText = styled.span`
+    font-size: 16px;
+`;
+export const FormFormik = styled.form`
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    margin: 0 auto;
+    margin-bottom: 20px;
+`;
+
+
+
+export const Input = styled.input`
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    font-size: 1.2rem;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+
+    &:focus {
+        outline: none;
+        border-color: #0088cc;
+    }
+
+    &:invalid {
+        border-color: #e74c3c;
+    }
+`;
